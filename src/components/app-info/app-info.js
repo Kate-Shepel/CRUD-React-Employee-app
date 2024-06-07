4fbd2dd86f59949e666cfd1ebf0fa445968c73f5
Kate-Shepel <kateborichevskaya@gmail.com>
2024-06-07 23:57:45 +0200

    refactor: translate to english

--- src/components/app-info/app-info.js
+++ src/components/app-info/app-info.js
@@ -3,9 +3,9 @@ import "./app-info.css";
 const AppInfo = ({employeeCount, onIncreaseCount}) => {
     return (
         <div className="app-info">
-            <h1>Учет сотрудников в компании N</h1>
-            <h2>Общее число сотрудников: {employeeCount}</h2>
-            <h2>Премию получат: {onIncreaseCount}</h2>
+            <h1>Employee Accounting at Company N</h1>
+            <h2>Total Number of Employees: {employeeCount}</h2>
+            <h2>Bonus Recipients: {onIncreaseCount}</h2>
         </div>
     )
 }
